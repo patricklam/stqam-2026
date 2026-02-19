d45d492e177c78865a418b52e92b43d9236dbdab
Patrick Lam <prof.lam@gmail.com>
2026-02-20 09:24:42 +1300

    fix property-based testing

--- assignments/a2/skel/a2-property-based-testing/custom_decimals.py
+++ assignments/a2/skel/a2-property-based-testing/custom_decimals.py
@@ -6,5 +6,5 @@ from hypothesis import given, strategies as st
 def custom_decimals(draw):
     pass
 
-def test_negate_twice_custom_decimals(d):
+def test_add_is_associative_custom_decimals(d1, d2, d3):
     pass
